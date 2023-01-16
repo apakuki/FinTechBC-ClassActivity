dec67bd3f9b0f01b711b440e66059ffcb1a09109
Samuel Nayacakalou <apakuki@pobox.com>
2023-01-16 21:42:39 +1100

    exercises

--- 18/1/Activities/04-Stu_A_Streamlit_App/Unsolved/st-app.py
+++ 18/1/Activities/04-Stu_A_Streamlit_App/Unsolved/st-app.py
@@ -7,12 +7,20 @@ import pandas as pd
 # Streamlit `write` function.
 # YOUR CODE HERE!
 
+st.write("# Welcome to FJ CBDC Wallet App")
+st.write("### This App will manage your FJDCoin")
+
 # @TODO: Create an opening sentence for your application using regular text and
 # the Streamlit `write` function.
 # YOUR CODE HERE!
+st.write("---")
+st.write("Bula Vinaka")
 
 # @TODO: Create a Pandas dataframe
 # YOUR CODE HERE!
+df = pd.DataFrame({'col1': [1, 2], 'col2': [3, 4]})
+st.write(df)
+
 
 # @TODO: Write the Pandas dataframe to the page
 # YOUR CODE HERE!
@@ -21,10 +29,15 @@ import pandas as pd
 # @TODO: Save the input as a variable.
 # YOUR CODE HERE!
 
+input_value = st.text_input("Enter a Message")
+
 # @TODO: Utilize the Streamlit `button` function to display the text input
 # variable created in the prior step to the page.
 # YOUR CODE HERE!
 
+if st.button("Display Message"):
+    st.write(input_value)
 
 # Bonus
 # YOUR CODE HERE!
+
