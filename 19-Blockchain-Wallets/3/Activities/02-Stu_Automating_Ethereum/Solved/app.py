ce4bcee6a1020384aec0a49da7a9c73a1d112e80
Samuel Nayacakalou <apakuki@pobox.com>
2023-02-09 14:23:15 +1100

    update

--- 19-Blockchain-Wallets/3/Activities/02-Stu_Automating_Ethereum/Solved/app.py
+++ 19-Blockchain-Wallets/3/Activities/02-Stu_Automating_Ethereum/Solved/app.py
@@ -2,14 +2,12 @@
 
 # Imports
 import streamlit as st
+
+# Import the functions from ethereum.py
+from ethereum import w3, generate_account, get_balance, send_transaction
 from web3 import Web3
 
 w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:7545'))
-
-# @TODO
-# Update the imports for the functions coming from ethereum.py
-from ethereum import generate_account
-
 
 ################################################################################
 # Streamlit Code
@@ -20,7 +18,7 @@ st.markdown("# Automating Ethereum with Streamlit!")
 #######################################
 
 # Generate the Ethereum account
-account = generate_account()
+account = generate_account(w3)
 
 #######################################
 
@@ -39,21 +37,29 @@ st.text("\n")
 st.text("\n")
 st.markdown("## Ethereum Account Balance:")
 
-# @TODO
-# Call the `get_balance` function and write the account balance to the screen
-
+# Call the get_balance function and write the account balance to the screen
+ether_balance = get_balance(w3, account.address)
+st.write(ether_balance)
 
 #######################################
 
 # An Ethereum Transaction
 st.text("\n")
 st.text("\n")
-st.markdown("## An Ethereum Transaction:")
+st.markdown("## An Ethereum Transaction")
 
-
-# @TODO
 # Create inputs for the receiver address and ether amount
+receiver = st.text_input("Input the receiver address")
+ether = st.number_input("Input the amount of ether")
 
-
-# @TODO
 # Create a button that calls the `send_transaction` function and returns the transaction hash
+if st.button("Send Transaction"):
+
+    transaction_hash = send_transaction(w3, account, receiver, ether)
+
+    # Display the Etheremum Transaction Hash
+    st.text("\n")
+    st.text("\n")
+    st.markdown("## Ethereum Transaction Hash:")
+
+    st.write(transaction_hash)
--- 19-Blockchain-Wallets/3/Activities/03-Stu_Cats_Mini_Project/Solved/cat_shop.py
+++ 19-Blockchain-Wallets/3/Activities/03-Stu_Cats_Mini_Project/Solved/cat_shop.py
@@ -1,9 +1,6 @@
-# Cats Mini Project
+# Streamlit as an Interface
 
 #In this part of the activity you will import the functions from your wallet.py file and create an interface using streamlit
-
-#In this portion of the activity we will create an interface and check our balance in ether to determine if we are able to purchase a cat.
-
 # Imports
 import streamlit as st
 from dataclasses import dataclass
@@ -12,8 +9,9 @@ from typing import Any, List
 
 
 
-# @TODO: # From crypto_wallet.py import w3, generate_account, get_balance
+# From crypto_wallet.py import w3, generate_account, get_balance
 
+from crypto_wallet import w3, generate_account, get_balance
 
 ################################################################################
 # Cat Information
@@ -34,37 +32,46 @@ kitties = ["Jennifurr", "Cheddar", "Meowise"]
 def get_cats():
     """Display the database of cats to purchase information."""
     db_list = list(cat_database.values())
-# Create a for loop through the cat_database use `db_list to produce the results`
+
     for number in range(len(kitties)):
-  # @TODO Use `st.write` to add the objects from the object to the code (hint price and name)
+        st.write("Name: ", db_list[number][0])
+        st.write("Price in Ether: ", db_list[number][1], "eth")
+        st.text(" \n")
 
 ################################################################################
 # Streamlit Code
 
-# @TODO Create Streamlit application headings using `st.markdown` to explain this app is for buying kitties
+# Create Streamlit application headings using `st.markdown` to explain this app is for buying kitties
+st.markdown("# Kitties")
+st.markdown("## Buy a Kitty!")
+st.text(" \n")
 
-# @TODO: Call the `generate_account` function and save it as a variable  called `account`.
+#  Call the `generate_account` function and save it as the variable `account`
+account = generate_account()
 
-# @TODO: Call the `get_balance` function and save it as the variable `ether`
+#  Call the `get_balance` function and save it as the variable `ether`
+ether = get_balance(account.address)
 
 # Disply the balance of ether in the account
 st.sidebar.markdown("## Your Balance of Ether")
 st.sidebar.markdown(ether)
 st.sidebar.markdown("---------")
 
+# Create a select box to chose a Cat using `st.sidebar.selectbox`
+cat = st.sidebar.selectbox('Select a cat', kitties)
 
-# @TODO: Create a select box to chose a Cat using `st.sidebar.selectbox`
-
-
-# @TODO: Create a header using ` st.sidebar.markdown()` to display cat name and price.
+#  Create a header using ` st.sidebar.markdown()` to display cat name and price.
+st.sidebar.markdown("## Cat Name and Price")
 
 # Identify the cat for purchase by name
 cat = cat_database[cat][0]
 
-#@TODO: Create a variable called cat_price to retrive the cat price
+# Create a variable called `cat_price` to retrive the cat price from the `cat_database` using block notation.
+cat_price = cat_database[cat][1]
 
-#@TODO: create an if else statement to check if the selected cat can be purchased
-
+# Use a conditional statement using the `if` keyword to check if the selected cat can be purchased. This will be done by checking the user's account balance that wishes to make the purchase.
+if cat_price <= ether:
+  new_balance = float(ether) - float(cat_price)
 # Write the cats name to the sidebar
   st.sidebar.write("If you buy", cat, "for", cat_price, "eth, your account balance will be", new_balance, ".")
   get_cats()
