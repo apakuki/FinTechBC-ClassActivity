2266c8d380f5e12806e051883adaca8d9bc63064
Samuel Nayacakalou <apakuki@pobox.com>
2023-02-16 21:10:02 +1100

    Week-22 Class 2 Activities

--- 22-dApps/2/Activities/03-Evr_Appraising_the_Situation/Unsolved/ArtRegistry/app.py
+++ 22-dApps/2/Activities/03-Evr_Appraising_the_Situation/Unsolved/ArtRegistry/app.py
@@ -54,11 +54,26 @@ st.markdown("## Register New Artwork")
 # 3. Initial appraisal value
 # 4. Artwork URI
 # @TODO: YOUR CODE HERE!
+artwork_name = st.text_input("Enter the name of the artwork")
+artist_name = st.text_input("Enter the artist name")
+initial_appraisal_value = st.text_input("Enter the initial appraisal amount")
+artwork_uri = st.text_input("Enter the URI to the artwork")
 
 # Create a button called "Register Artwork" that uses the contract's
 # registerArtwork function to register new artwork.
 # Display the receipt for the transaction on the webpage.
 # @TODO: YOUR CODE HERE!
+if st.button("Register Artwork"):
+    tx_hash = contract.functions.registerArtwork(
+        address,
+        artwork_name,
+        artist_name,
+        int(initial_appraisal_value),
+        artwork_uri
+    ).transact({'from': address, 'gas': 1000000})
+    receipt = w3.eth.waitForTransactionReceipt(tx_hash)
+    st.write("Transaction receipt mined:")
+    st.write(dict(receipt))
 st.markdown("---")
 
 
@@ -76,6 +91,11 @@ if st.button("Appraise Artwork"):
     # HINT: You can use the first account in `w3.eth.accounts[0]` for the
     # transaction.
     # @TODO: YOUR CODE HERE!
+    tx_hash = contract.functions.newAppraisal(
+        token_id,
+        int(new_appraisal_value),
+        report_uri
+    ).transact({"from": w3.eth.accounts[0]})
     receipt = w3.eth.waitForTransactionReceipt(tx_hash)
     st.write(receipt)
 st.markdown("---")
@@ -84,11 +104,25 @@ st.markdown("---")
 # Get Appraisals
 ################################################################################
 st.markdown("## Get the appraisal report history")
+art_token_id = st.number_input("Artwork ID", value=0, step=1)
 # Create a streamlit component that inputs a artwork token id from the user
 # @TODO: YOUR CODE HERE!
 if st.button("Get Appraisal Reports"):
     # Create a filter that lists all of the Appraisal events for the token.
     # @TODO: YOUR CODE HERE!
-
+    appraisal_filter = contract.events.Appraisal.createFilter(
+        fromBlock=0,
+        argument_filters={"tokenId": art_token_id}
+    )
+    appraisals = appraisal_filter.get_all_entries()
     # Use streamlit to display all entries from the event filter.
     # @TODO: YOUR CODE HERE!
+    if appraisals:
+        for appraisal in appraisals:
+            report_dictionary = dict(appraisal)
+            st.markdown("### Appraisal Report Event Log")
+            st.write(report_dictionary)
+            st.markdown("### Appraisal Report Details")
+            st.write(report_dictionary["args"])
+    else:
+        st.write("This artwork has no new appraisals")    
